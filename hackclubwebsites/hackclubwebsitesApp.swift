1fe5ff0fd48d2acc5f5bdef7f9d58ad58f661b73
potato dog man <saumil707@gmail.com>
2024-11-08 07:46:55 +0800

    Initial Commit

---
+++ hackclubwebsites/hackclubwebsitesApp.swift
@@ -0,0 +1,17 @@
+//
+//  hackclubwebsitesApp.swift
+//  hackclubwebsites
+//
+//  Created by T Krobot on 8/11/24.
+//
+
+import SwiftUI
+
+@main
+struct hackclubwebsitesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
